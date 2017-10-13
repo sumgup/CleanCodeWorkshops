f4a22db52db2557eb3a194acaa2616fbf8f29560
Ranjith <ranjithyk.yk@gmail.com>
2017-10-13 17:58:26 +0530

    Added DI demo solutions by Raghavendhra

---
+++ Tracks/SOLID_Design_Principles/WeekThree-LSP-ISP-DIP/DIP/DependencyInversionPrinciple/Dependency-InversionPrinciple/BOABankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_InversionPrinciple
+{
+    public class BOABankAccount : ITransferSource, ITransferDestination
+    {
+        public long AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+
+        public void AddFunds(decimal value)
+        {
+            Balance += value;
+        }
+        public void RemoveFunds(decimal value)
+        {
+            Balance -= value;
+        }
+    }
+}
